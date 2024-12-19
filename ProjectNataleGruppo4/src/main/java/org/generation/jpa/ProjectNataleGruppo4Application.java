07e9d3fafb0888899279ffaaba03fb63f3affae5
Stefano-Muzio <stefano.muzio18@gmail.com>
2024-12-19 11:17:53 +0100

    creazione progetto

---
+++ ProjectNataleGruppo4/src/main/java/org/generation/jpa/ProjectNataleGruppo4Application.java
@@ -0,0 +1,13 @@
+package org.generation.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNataleGruppo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNataleGruppo4Application.class, args);
+	}
+
+}
---
+++ ProjectNataleGruppo4/src/test/java/org/generation/jpa/ProjectNataleGruppo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNataleGruppo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
