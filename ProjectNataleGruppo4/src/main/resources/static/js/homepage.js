9c89a203be6435b41381926f6684a40a52a2948f
michelecaldarelli <183734557+michelecaldarelli@users.noreply.github.com>
2024-12-19 21:49:03 +0100

    Update v 0.0.3 - modifica span su h1 - carosello swiper da aggiustare - js implementato per il carosello

---
+++ ProjectNataleGruppo4/src/main/resources/static/js/homepage.js
@@ -0,0 +1,23 @@
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  }
+});
