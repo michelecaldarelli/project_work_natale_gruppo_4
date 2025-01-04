f65c56ccbdd77c43dfc2b1553103bce1385379bf
Castrese Caiazzo <castresecaiazzo75@gmail.com>
2025-01-04 18:43:06 +0100

    lista prenotazioni abbellita
    ste sarà il padre dei miei bambini

--- ProjectNataleGruppo4/src/main/resources/static/js/utente.js
+++ ProjectNataleGruppo4/src/main/resources/static/js/utente.js
@@ -12,17 +12,27 @@ fetch('http://localhost:8099/login/userData')
         fetch(`http://localhost:8099/api/utente/id:${data.id}`)
             .then(res => res.json())
             .then(data => {
-                let prenotazioni = data.prenotazioni
+                let prenotazioni = data.prenotazioni;
                 prenotazioni.forEach(p => {
                     fetch(`http://localhost:8099/api/veicolo/${p.veicoloId}`)
                         .then(res => res.json())
                         .then(data => {
-                            listaPrenotazioni.innerHTML += `<li>${data.marca} - ${data.modello}</li>`;
+                            
+                            // HO USATO IL CLASSLIST PER BOOTSTRAP STEEE
+                            const listItem = document.createElement('li');
+                            listItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+                            
+                            listItem.innerHTML = `
+                                <div>${data.marca} - ${data.modello}</div>
+                                <button class="btn">
+                                    <i class="fa-regular fa-trash-can"></i> 
+                                </button>
+                            `;
 
-                        })
+                            listaPrenotazioni.appendChild(listItem);
+                        });
                 });
             })
+    });
 
 
-
-    })
